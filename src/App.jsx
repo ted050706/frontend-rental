f453474168649dd8357cc70be1b99b6df2f7655f
ted050706 <ted050706>
2026-07-18 11:47:03 +0800

    react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* L30-1 */
 /**
  * 路徑:/                   模組:<ItemsPage />             租用項目
  * 路徑:/login              模組:<LoginPage />             登入項目
@@ -8,66 +9,15 @@
  * 安裝 React Router
  * 指令 npm install react-router-dom
  */
-
-import { Route, Routes } from "react-router-dom"
-
-// 匯入 Navbar
-import Navbar from "./components/Navbar"
-
-// 匯入 ProtectedRoute
-import ProtectedRoute from "./components/ProtectedRoute"
-
-import ItemsPage from './pages/ItemsPage'
-import LoginPage from './pages/LoginPage'
-import MyReservationsPage from './pages/MyReservationsPage'
-import AdminItemsPage from './pages/AdminItemsPage'
-import AdminReservationsPage from './pages/AdminReservationsPage'
-
 function App() {
   return (
-    <>
-      {/* Navbar 放在 Routers 的外面代表每一頁都可以看得到 */}
-      <Navbar />
+    <div>
+      {/* 頁面主標題 */}
+      <h1>租用預約系統</h1>
 
-      <main className="container">
-        {/* Routes 代表路由區域 */}
-        <Routes>
-          {/* 首頁(租用項目) */}
-          <Route path="/" element={<ItemsPage />} />
-          
-          {/* 登入頁 */}
-          <Route path="/login" element={<LoginPage />} />
-
-          {/* 我的預約頁: 需要登入 */}
-          <Route 
-            path="/my-reservations" 
-            element={
-              <ProtectedRoute>
-                <MyReservationsPage />
-              </ProtectedRoute>
-            } />
-
-          {/* 管理者-項目管理頁 */}
-          <Route 
-            path="/admin/items" 
-            element={
-              <ProtectedRoute adminOnly>
-                <AdminItemsPage />
-              </ProtectedRoute>
-            } />
-
-          {/* 管理者-預約審核頁 */}
-          <Route 
-            path="/admin/reservations" 
-            element={
-              <ProtectedRoute adminOnly>
-                <AdminReservationsPage />
-              </ProtectedRoute>
-            } />
-        </Routes>
-
-      </main>
-    </>
+      {/* 頁面說明文字 */}
+      <p>歡迎使用 React 租用預約系統</p>
+    </div>
   )
 
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ItemsPage from './pages/ItemsPage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import MyReservationPage from './pages/MyReservationPage.jsx'
+import AdminItemsPage from './pages/AdminItemsPage.jsx'
+import AdminReservationsPage from './pages/AdminReservationsPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
